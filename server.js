658aeab3e794db0dcdeaf0287869a9acd1b59e2d
Faizan Chaudary <83314898+faizan-debug@users.noreply.github.com>
2024-08-26 01:04:25 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json({ extended: false}));
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({msg: "This is root" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server has been started\nhttp://localhost:${PORT}`);
+});
